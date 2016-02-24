c5f8448fed8f87a9343d3ab9705b1b463bd508ba
rjagiasi <rjagiasi@gmail.com>
2016-02-24 13:33:18 +0530

    half built

---
+++ html/student.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<link rel="stylesheet" href="../css/bootstrap.min.css">		
+		<script src="../js/jquery.min.js"></script>
+		<script src="../js/bootstrap.min.js"></script>
+
+		<style>
+		#footer {
+			background-color:black;
+			color:white;
+			clear:both;
+			text-align:center;
+			padding:5px;
+			bottom:0;
+			width:100%;
+		}
+		
+		#menu {
+			width:18%;
+			float: left;
+			margin: 1%;
+		}
+		#menu .menuheads{
+			text-align: center;
+		}
+		#menu .menuheads:hover{
+			background-color: #555;
+			color: white;
+		}
+		.sectionbranches{
+			background-color: #f2f2f2;
+		}
+		#content{
+			width:76%;
+			float: right;
+			margin: 2%;
+			overflow: auto;
+		}
+		</style>
+		
+		<script type="text/javascript">
+		$(document).ready(function() {
+
+			$("#content").html("<p>Select a class</p>");
+			$("#enggbranches").collapse("show");
+			$("#enggbranches ul").not("#" + "<?=$_GET["branch"]?>").collapse("hide");
+
+			$("#menu").find("a").click(function(event) {
+				var classid = $(event.target).parent().attr("id");
+				var branchid = $(event.target).parent().parent().attr("id");
+				setactiveclass(classid, branchid);
+			});
+		});
+		
+		function setactiveclass(classid, branchid) {
+			$(".active").toggleClass("active", false);
+			$("#enggbranches ul").not("#" + branchid).collapse("hide");
+			$("#enggbranches").collapse("show");
+			$("#" + classid).addClass("active");
+			$("#content").html("<p>Class Selected : "+ classid.toUpperCase() +"</p>");
+		}
+
+
+		</script>
+		
+		<img style="float:left" src="../imgs/logo.png" alt = "Ves Logo" >
+		<center><h3>VESIT Attendance Portal</h3></center>
+		<hr/>
+		<center>
+		<p>For authorized Users only</p>
+		<form class = "form-inline" action = "staff.html" id="login-form">
+			<input name="usrname" class="form-control" placeholder="Username" type = "text"/>
+			<input name="password" class="form-control" placeholder="Password" type = "password"/>
+			<button class = "btn btn-default">Submit</button>
+		</form>
+		</center>
+		
+		
+	</head>
+	<body>
+		
+		<br/><br/><br/>
+		
+		<div id="menu">
+			<ul class="nav nav-pills nav-stacked">
+				<li class="menuheads">MCA</li>
+				<li class="menuheads">Diploma</li>
+				<li data-toggle="collapse" data-target="#enggbranches" class="menuheads collapsed" aria-expanded="false">Engineering</li>
+				<div class="sectionbranches collapse" id="enggbranches" aria-expanded="false" style="height:0px;">
+					<li data-toggle="collapse" data-target="#it" class="menuheads" id="itheader" aria-expanded="true">IT</li>
+					<ul id="it" class="nav nav-pills nav-stacked collapse in" aria-expanded="true">
+						<li id="d10"><a>D10</a></li>
+						<li id="d15"><a>D15</a></li>
+					</ul>
+					<li data-toggle="collapse" data-target="#comps" class="menuheads" id="compsheader" aria-expanded="true">Comps</li>
+					<ul id="comps" class="nav nav-pills nav-stacked collapse in" aria-expanded="true">
+						<li id="d7a"><a>D7A</a></li>
+						<li id="d7b"><a>D7B</a></li>
+					</ul>
+				</div>
+			</ul>
+		</div>
+		
+		<div id="content">
+
+		</div>
+		
+		<div id="footer">
+			Copyright &copy; vesit.edu
+		</div>
+	</body>
+</html>
