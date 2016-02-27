2e7bfc634d5cf295519adc4df2edc4a730354364
rjagiasi <rjagiasi@gmail.com>
2016-02-27 16:13:40 +0530

    Staff registration added

--- html/student.php
+++ html/student.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-
+		<title>VESIT : Student</title>
 		<link rel="stylesheet" href="../css/bootstrap.min.css">		
 		<script src="../js/jquery.min.js"></script>
 		<script src="../js/bootstrap.min.js"></script>
